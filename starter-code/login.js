15a234a43a8e668b5ef1f505219745a1316d19b6
ccpinotti <ccpinotti@gmail.com>
2017-06-29 16:38:59 -0700

    have code for 2 of 3 but not one is not operating

--- starter-code/login.js
+++ starter-code/login.js
@@ -1,1 +1,16 @@
 console.log("login.js loaded");
+let userLogin = {
+  userName : 'Chris12',
+  passWord : 'Giants'
+}
+
+  function login () {
+    prompt("Enter Password for user Chris12");
+    if ( "" === userLogin.passWord) {
+    return alert("correct message")
+  } else {
+    return alert("wrong answer")
+  }
+
+  login();
+ }
--- starter-code/security_questions.js
+++ starter-code/security_questions.js
@@ -1,1 +1,34 @@
 console.log("security_questions.js loaded");
+//let securityQuestions = ["Where were you born?", "What is your favorite sport", "Mother's maiden name?", "What is your job", "What is your eye color" ];
+//let expectedAnswer = ["San Francisco", "Baseball", "Smith", "Engineer", "Blue"];
+let securityQuestions = [{
+  question: "Where were you born?",
+  expectedAnswer: "San Francsico"
+}, {
+  question: "What is your favorite sport?",
+  expectedAnswer: "Baseball"
+}, {
+  question: "Mother's maiden name?",
+  expectedAnswer: "Smith"
+}]
+
+let answer = "";
+
+
+for (var i = 0; i > securityQuestions.length; i++) {
+  let answer = prompt(securityQuestions[i].question);
+  if (securityQuestions[i] === expectedAnswer[0]) {
+
+    securityQuestions = alert("Correct!")
+  } else {
+    securityQuestions = alert("Wrong Answer")
+
+  }
+}
+
+
+
+
+
+
+//if input matches expected answer end loop, if it doesnt match keep asking
